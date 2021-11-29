ed51b5dbc3ff730f75d43b4caca9e001dd04ebb4
YoATW9 <82594032+YoATW9@users.noreply.github.com>
2021-11-29 13:55:10 +0800

    Update server.js
    added login part in L10 notes, will edit later

--- server.js
+++ server.js
@@ -1,14 +1,71 @@
+
+const express = require('express');
+const session = require('cookie-session');
+const bodyParser = require('body-parser');
+const app = express();
 const ObjectID = require('mongodb').ObjectID;
 const assert = require('assert');
 const http = require('http');
 const url = require('url');
 
-const mongourl = '';
+const mongourl = 'mongodb+srv://Admin:Admin@cluster.yhbdv.mongodb.net/inventory?retryWrites=true&w=majority';
 const mongoose = require('mongoose');
 const bookingSchema = mongoose.Schema({ 
     bookingid: String,
     mobile: String
 });
+
+app.set('view engine','ejs');
+
+const SECRETKEY = 'I want to pass COMPS381F';
+
+const users = new Array(
+	{name: 'developer', password: 'developer'},
+	{name: 'guest', password: 'guest'}
+);
+
+app.set('view engine','ejs');
+
+app.use(session({
+  name: 'loginSession',
+  keys: [SECRETKEY]
+}));
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req,res) => {
+	console.log(req.session);
+	if (!req.session.authenticated) {    // user not logged in!
+		res.redirect('/login');
+	} else {
+		res.status(200).render('secrets',{name:req.session.username});
+	}
+});
+
+app.get('/login', (req,res) => {
+	res.status(200).render('login',{});
+});
+
+app.post('/login', (req,res) => {
+	users.forEach((user) => {
+		if (user.name == req.body.name && user.password == req.body.password) {
+			// correct user name + password
+			// store the following name/value pairs in cookie session
+			req.session.authenticated = true;        // 'authenticated': true
+			req.session.username = req.body.name;	 // 'username': req.body.name		
+		}
+	});
+	res.redirect('/');
+});
+
+app.get('/logout', (req,res) => {
+	req.session = null;   // clear cookie-session
+	res.redirect('/');
+});
+
+app.listen(process.env.PORT || 8099);
 
 const handle_Find = (res, criteria) => {
     mongoose.connect(mongourl, {useMongoClient: true});
